dc339cb17679227e8c9201eacc8611c8fb8d49dc
Yuri Novokreschenov <yuri.novokreschenov@gmail.com>
2024-02-23 14:43:42 +0300

    sprint 6 v1 syntax check fix 8

--- src/model/Task.java
+++ src/model/Task.java
@@ -8,7 +8,7 @@ public class Task {
     private String title;
     private String description;
 
-    public Task(){
+    public Task() {
     }
 
     public Task(String title, String description, TaskState taskState) {
