8172e0b746458c9f706a6c99d8ec042ec436d062
Yuri Novokreschenov <yuri.novokreschenov@gmail.com>
2024-02-23 14:19:53 +0300

    sprint 6 v1 syntax check fix 2

--- src/service/Managers.java
+++ src/service/Managers.java
@@ -1,11 +1,11 @@
 package service;
 
 public class Managers {
-    public static TaskManager getDefaultTaskManager(){
+    public static TaskManager getDefaultTaskManager() {
         return new InMemoryTaskManager(new InMemoryHistoryManager());
     }
 
-    public static HistoryManager getDefaultHistory(){
+    public static HistoryManager getDefaultHistory() {
         return new InMemoryHistoryManager();
     }
 }
